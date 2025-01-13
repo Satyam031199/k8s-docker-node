7186e2e1d56fcbb5d7069b8a264bd76a72f4dd8b
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2025-01-13 15:31:58 +0530

    add /nginx route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import os from "os";
+import fetch from "node-fetch";
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,13 @@ app.get("/", (req, res) => {
   const msg = `VERSION: 2 Hello from ${os.hostname()}!`;
   console.log(msg);
   res.send(msg);
+});
+
+app.get("/nginx", async (req, res) => {
+  const url = "http://nginx";
+  const response = await fetch(url);
+  const data = await response.text();
+  res.send(data);
 });
 
 app.listen(PORT, () => {
